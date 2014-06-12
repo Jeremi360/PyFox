84e3f69dc2d35ddfbfc7f4c145edd7ec69659028
Jeremi Biernacki <jeremi360@gmail.com>
2014-06-12 14:48:49 +0200

    / ‘find.py’

--- find.py
+++ find.py
@@ -13,21 +13,12 @@ class Find(Gtk.HBox):
         close_button.connect("clicked", lambda x: self.hide())
 
         self.find_entry = self.ui.get_object("Fentry")
-        self.find_entry.connect("activate",
-                                lambda x: tab.webview.search_text(
-                                tab.find_entry.get_text(),
-                                False, True, True))
+        self.find_entry.connect("activate", lambda x: tab.webview.search_text(tab.find_entry.get_text(), False, True, True))
 
         prev_button = self.ui.get_object("Fback")
-        prev_button.connect("clicked",
-                            lambda x: tab.webview.search_text(
-                            tab.find_entry.get_text(),
-                            False, False, True))
+        prev_button.connect("clicked", lambda x: tab.webview.search_text(tab.find_entry.get_text(), False, False, True))
 
         next_button = self.ui.get_object("Fnext")
-        next_button.connect("clicked",
-                            lambda x: tab.webview.search_text(
-                            tab.find_entry.get_text(),
-                            False, True, True))
+        next_button.connect("clicked", lambda x: tab.webview.search_text(tab.find_entry.get_text(), False, True, True))
 
 
