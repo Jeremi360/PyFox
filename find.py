dca4f20230c022ff5831fb10961461ab2a3c9ee7
Jeremi Biernacki <jeremi360@gmail.com>
2014-06-12 13:46:41 +0200

    / ‘find.py’

--- find.py
+++ find.py
@@ -1,22 +1,24 @@
+from gi.repository import Gtk, GdkPixbuf, WebKit
+import os, sys
 
 class Find(Gtk.HBox):
-    def __init__(self):
+    def __init__(self, tab):
         close_button = Gtk.Button("Close")
-        close_button.connect("clicked", lambda x: find_box.hide())
+        close_button.connect("clicked", lambda x: self.hide())
         self.find_entry = Gtk.Entry()
         self.find_entry.connect("activate",
-                                lambda x: self.webview.search_text(self.find_entry.get_text(),
+                                lambda x: tab.webview.search_text(tab.find_entry.get_text(),
                                                                    False, True, True))
         prev_button = Gtk.Button("Previous")
         next_button = Gtk.Button("Next")
         prev_button.connect("clicked",
-                            lambda x: self.webview.search_text(self.find_entry.get_text(),
+                            lambda x: tab.webview.search_text(tab.find_entry.get_text(),
                                                                False, False, True))
         next_button.connect("clicked",
-                            lambda x: self.webview.search_text(self.find_entry.get_text(),
+                            lambda x: tab.webview.search_text(tab.find_entry.get_text(),
                                                                False, True, True))
-        find_box.pack_start(close_button, False, False, 0)
-        find_box.pack_start(self.find_entry, False, False, 0)
-        find_box.pack_start(prev_button, False, False, 0)
-        find_box.pack_start(next_button, False, False, 0)
-        self.find_box = find_box
+
+        self.pack_start(url_box, False, False, 0)
+        self.pack_start(scrolled_window, True, True, 0)
+        self.pack_start(find_box, False, False, 0)
+
