f6406f1cca31091e93c1e1d208fcaaee9448cb67
Jeremi Biernacki <jeremi360@gmail.com>
2014-06-12 19:17:17 +0200

    / ‘tab.py’

--- tab.py
+++ tab.py
@@ -68,7 +68,7 @@ class Tab(Gtk.VBox):
 
 	def load_icon(self, webview, url):
 		try:
-			f = urllib.parse(url)
+			f = urllib.open(url)
 			data = f.read()
 			pixbuf_loader = GdkPixbuf.PixbufLoader()
 			pixbuf_loader.write(data)
