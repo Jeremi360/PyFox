e10946d3652a138190f93fcdf6d720056e08ba8e
Jeremi Biernacki <jeremi360@gmail.com>
2014-06-12 13:53:07 +0200

    / ‘find.py’

--- find.py
+++ find.py
@@ -5,21 +5,27 @@ UI_find = os.path.join("ui", "Find.ui")
 
 class Find(Gtk.HBox):
     def __init__(self, tab):
+        self.ui = Gtk.Builder()
+        self.ui.add_from_file(UI_find)
+        self.ui.connect_signals(self)
 
-        close_button = Gtk.Button("Close")
+        close_button = self.ui.get_object("Fclose")
         close_button.connect("clicked", lambda x: self.hide())
-        self.find_entry = Gtk.Entry()
+
+        self.find_entry = self.ui.get_object("Fentry")
         self.find_entry.connect("activate",
                                 lambda x: tab.webview.search_text(tab.find_entry.get_text(),
-                                                                   False, True, True))
-        prev_button = Gtk.Button("Previous")
-        next_button = Gtk.Button("Next")
+                                False, True, True))
+
+        prev_button = self.ui.get_object("Fback")
         prev_button.connect("clicked",
                             lambda x: tab.webview.search_text(tab.find_entry.get_text(),
-                                                               False, False, True))
+                            False, False, True))
+
+        next_button = self.get_object("Fnext")
         next_button.connect("clicked",
                             lambda x: tab.webview.search_text(tab.find_entry.get_text(),
-                                                               False, True, True))
+                            False, True, True))
 
         self.pack_start(url_box, False, False, 0)
         self.pack_start(scrolled_window, True, True, 0)
