8875635dc701f90e329645d0bb555a3dc1737b14
Jeremi Biernacki <jeremi360@gmail.com>
2014-06-14 21:14:04 +0200

    / ‘main.py’

--- main.py
+++ main.py
@@ -13,3 +13,25 @@ class Browser(object):
         self.ui.add_from_file(UI_FILE)
         self.ui.connect_signals(self)
 
+        #get objects from UI_FILE
+        self.Tabs = self.ui.get_object("tabs")
+        self.TabButton = self.ui.get_object("TabButton")
+        self.MenuButton = self.ui.get_object("MenuButton")
+        self.Add = self.ui.get_object("Add")
+        self.Downs = self.ui.get_object("Downs")
+        self.Full = self.ui.get_object("Full")
+        self.TabsBox = self.ui.get_object("TabsBox")
+
+        #TabButton - is temple
+        self.TabButton.hide()
+
+        #hide this - don't work yet:
+        self.MenuButton.hide()
+        self.Downs.hide()
+        self.Full.hide()
+
+
+    def new_tab(self):
+        view = Tab().box
+        #self.TabsBox.
+
