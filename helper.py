a77017844b25ebd85d449b90da6167553fed404f
Jeremi Biernacki <jeremi360@gmail.com>
2014-06-16 18:37:26 +0200

    / ‘helper.py’

--- helper.py
+++ helper.py
@@ -4,14 +4,11 @@ from gi.repository import Gtk
 class Window(Gtk.Window):
     def __init__(self, content):
         super(Window, self).__init__()
-        self.do_when_init()
         self.set_size_request(400, 400)
         self.connect("destroy", Gtk.main_quit)
         self.add(content)
         self.show()
 
-    def do_when_init(self):
-        pass
 
 class Builder(object):
     def __init__(self, UI_FILE):
