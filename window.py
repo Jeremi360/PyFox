231e566fddc760d603340d88acd5b2cc8869ff40
Jeremi Biernacki <jeremi360@gmail.com>
2014-06-16 21:44:30 +0200

    / ‘window.py’

--- window.py
+++ window.py
@@ -12,56 +12,17 @@ class Builder(object):
         self.ui.add_from_file(UI_FILE)
         self.ui.connect_signals(self)
 
-UI_WC = os.path.join("ui", "WindowControls.ui")
-
 class Window(Builder, Gtk.Window):
-    def __init__(self, content = Gtk.Box(), wc_box = None):
+    def __init__(self, content = Gtk.Box()):
         Gtk.Window.__init__(self)
-        Builder.__init__(self, UI_WC)
-
-        self.wc = self.ui.get_object("box")
         self.content = content
         self.do_then_init()
-
-        if wc_box == None:
-            self.add_wc(self.content)
-            '''
-            try:
-                self.add_wc(self.content)
-
-            except:
-                self.set_decorated(True)
-                self.connect("destroy", lambda x: Gtk.main_quit())
-            '''
-
-        else:
-            self.add_wc(wc_box)
 
         self.set_size_request(400, 400)
         self.add(self.content)
         self.maximize()
         self.set_icon_from_file(Logo)
         self.show()
-
-    def mac(self):
-        try:
-            self.maximize()
-        except:
-            self.unmaximize()
-
-    def add_wc(self, wc_box):
-        close = self.ui.get_object("close")
-        close.connect("clicked", lambda x: Gtk.main_quit())
-
-        maks = self.ui.get_object("max")
-        maks.connect("clicked", lambda x: self.mac())
-
-        mini = self.ui.get_object("mini")
-        mini.connect("clicked", lambda x: self.iconify())
-
-        self.set_decorated(False)
-        wc_box.pack_end(self.wc, False, False, 0)
-        self.wc.show()
 
     def do_then_init(self):
         pass
