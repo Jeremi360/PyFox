42d92f2258ba1713dda44f1ea45b78ed7211834b
Jeremi Biernacki <jeremi360@gmail.com>
2014-06-16 12:06:21 +0200

    / ‘helper.py’

--- helper.py
+++ helper.py
@@ -9,9 +9,9 @@ class Window(Gtk.Window):
         self.add(content)
         self.show()
 
-class Group(object):
-    def __init__(self):
+class Builder(object):
+    def __init__(self, UI_FILE):
         #load UI from UI_FILE
         self.ui = Gtk.Builder()
-        self.ui.add_from_file(UI_Group)
+        self.ui.add_from_file(UI_FILE)
         self.ui.connect_signals(self)
