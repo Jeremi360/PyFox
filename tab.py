3eda644b002a99228c09eedab192ef0f9efdd849
Jeremi Biernacki <jeremi360@gmail.com>
2014-06-19 16:53:09 +0200

    / ‘tab.py’

--- tab.py
+++ tab.py
@@ -24,7 +24,7 @@ class TabButton(Garbbo.Builder):
 	def get(self):
 		return self.ui.get_object("box")
 
-	def toggled(self):
+	def toggled(self, , button, name):
 		n = self.group.tabs.get_current_page()
 		t = self.group.tabs.page_num(self.tab.get())
 
