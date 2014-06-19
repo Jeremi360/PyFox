2755f86e55f9809ed766179217708d55c143e3ae
Jeremi Biernacki <jeremi360@gmail.com>
2014-06-19 15:49:50 +0200

    + ‘Grabbo/Window.py’

---
+++ Grabbo/Window.py
@@ -0,0 +1,17 @@
+from gi.repository import Gtk
+from Builder import Builder 
+
+class Window(Builder, Gtk.Window):
+    def __init__(self, content = Gtk.Box()):
+        Gtk.Window.__init__(self)
+        self.content = content
+        self.do_then_init()
+
+        self.set_size_request(400, 400)
+        self.add(self.content)
+        self.maximize()
+        self.set_icon_from_file(Logo)
+        self.show()
+
+    def do_then_init(self):
+        pass
