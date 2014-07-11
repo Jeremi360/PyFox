38f614a2deeb6f88e6c16dcebabf7ce35464fbce
Jeremi Biernacki <jeremi360@gmail.com>
2014-07-12 00:58:49 +0200

    / ‘crowbar/tab.py’

--- crowbar/tab.py
+++ crowbar/tab.py
@@ -117,7 +117,8 @@ class Tab(grabbo.Builder):
 			pixbuf = webview.get_icon_pixbuf()
 			self.urlen.set_icon_from_pixbuf(Gtk.EntryIconPosition.PRIMARY, pixbuf)
 			img = Gtk.Image()
-			img.set_from_pixbuf(pixbuf, 4)
+			img.set_from_pixbuf(pixbuf)
+			img.set_size_request(16, 16)
 			self.TB.button.set_image(img)
 
 		except:
