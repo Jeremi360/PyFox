e77ee387e2465e4de61a3a0212e0b8d8869683b0
Jeremi Biernacki <jeremi360@gmail.com>
2014-07-11 22:58:31 +0200

    / ‘crowbar.py’ / ‘tab.py’

--- crowbar.py
+++ crowbar.py
@@ -10,10 +10,9 @@ UI_Group = os.path.join("ui", "Group.ui")
 class Group(grabbo.Builder, grabbo.Notebook):
     def __init__(self, parent):
         grabbo.Builder.__init__(self, UI_Group)
-        grabbo.Notebook.__init__(content = {Tab():"New Page"}, addable_content = (Gtk.Label("New Content"), "New Page"), addable = True, closeable = True, orientation = Gtk.Orientation.HORIZONTAL):
+        grabbo.Notebook.__init__(content = {Tab():"New Page"}, addable_content = (Tab(), "New Page"), addable = True, closeable = True, orientation = Gtk.Orientation.HORIZONTAL):
 
         self.parent = parent
-
         #get objects from UI_FILE
         self.menub = self.ui.get_object("MenuButton")
         self.add = self.ui.get_object("Add")
