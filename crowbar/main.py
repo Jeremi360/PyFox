eec22402e627d7d6984cea2286f78e0ecad5d2ef
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-12 15:54:02 +0200

    / ‘crowbar/main.py’

--- crowbar/main.py
+++ crowbar/main.py
@@ -44,7 +44,7 @@ class Group(grabbo.Builder):
         self.tabs = Tabs(self)
         self.box.add(self.tabs.get())
         self.tabs.get().show()
-        self.box.pack_end(self.tabs.pages, True, True, 0)
+        #self.box.pack_end(self.tabs.pages, True, True, 0)
         self.box.show()
 
     def set_title(self, text):
