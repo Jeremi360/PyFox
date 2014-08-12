60d56f919d4aab421621b7b2725536716c28e7d8
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-12 12:38:34 +0200

    / ‘crowbar/extensions/totop.py’

--- crowbar/extensions/totop.py
+++ crowbar/extensions/totop.py
@@ -5,7 +5,9 @@ Name = "ToTop"
 shortDes = "Go To Top of page",
 Descrption = "and nothing more :P",
 Author = "Jeremi 'jeremi360' Biernacki"
-url = ""
+url_1 = "https://github.com/jeremi360/cRoWBaR"
+url_2 = "/blob/master/crowbar/extensions/totop.py"
+url = "".join(url_1, url_2)
 Icon = Gtk.STOCK_GOTO_TOP
 
 class ToTop(Extension):
