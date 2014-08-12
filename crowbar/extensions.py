d53a79d7478b5f96526be9fe1ea47fa12691cae1
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-12 11:53:50 +0200

    / ‘crowbar/extensions.py’

--- crowbar/extensions.py
+++ crowbar/extensions.py
@@ -4,18 +4,49 @@ import os
 
 ExtBox_UI = os.path.join('..', 'ui', 'ExtBox.ui')
 
+class ExtBox(Builder):
+    def __init__(self, ext):
+        super(ExtBox, self).__init__(ExtBox_UI)
+        self._label = self.ui.get_object("label")
+        self._icon = self.ui.get_object("image")
+        self._switch = self.ui.get_object("switch")
+        self._des = self.ui.get_object("des")
+        self._desEx = self.ui.get_object("desEx")
+        self._link = self.ui.get_object("link")
+        self._roz = self.ui.get_object("roz")
+        self._zwi = self.ui.get_object("zwi")
+        self._
+
+        self._label.set_text(ext.get_name())
+        self._des.set_test(ext.get_short_descrption())
+        self._desEX.set_text(ext.get_descrption())
+        self._icon.set_image(ext.get_icon())
+        self._link.set_text("Autor's page")
+        self._link.set_link(ext.get_url())
+
+
+
+    def get(self):
+        return self.ui.get_object("box")
+
+
 class Extension(object):
-    def __init__(self, Name, Descrption, Author, url, Icon):
+    def __init__(self, Name, shortDes, Descrption, Author, url, Icon):
         self._name = Name
         self._des = Descrption
+        self._short = shortDes
         self._aur = Author
         self._url = url
         self._icon = Icon
+        self._box = ExtBox(self)
 
     def get_name(self):
         return self._name
 
     def get_descrption(self):
+        return self._des
+
+    def get_short_descrption(self):
         return self._des
 
     def get_author(self):
@@ -27,16 +58,6 @@ class Extension(object):
     def get_icon(self):
         return self._icon
 
-
-class ExtBox(Builder):
-    def __init__(self, ext = Extension):
-        super(ExtBox, self).__init__(ExtBox_UI)
-        self._label = self.ui.get_object("label")
-        self._icon = self.ui.get_object("image")
-        self._switch = self.ui.get_object("switch")
-        self._text = self.ui.get_object("textview")
-
-        self._label Gtk.Label.set_text(ext.)
 
 
 
