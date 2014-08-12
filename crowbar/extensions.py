3774be921d3a2c9cca2c4072660173396eae99be
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-12 11:11:04 +0200

    / ‘crowbar/extensions.py’

--- crowbar/extensions.py
+++ crowbar/extensions.py
@@ -1,3 +1,23 @@
 from gi.repository import Gtk
+from grabbo import Builder
+import os
 
-class Ext:
+ExtBox_UI = os.path.join('..', 'ui', 'ExtBox.ui')
+
+class ExtBox(Builder):
+    def __init__(self, ext):
+        super(ExtBox, self).__init__(ExtBox_UI)
+
+
+class Extension(object):
+    def __init__(self, Name, Descrption, Author, url, Icon):
+        self._name = Name
+        self._des = Descrption
+        self._aur = Author
+        self._url = url
+        self._icon = Icon
+
+        self._box =
+
+        self._
+
