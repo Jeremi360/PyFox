fb745d968c9ded5a63e342c5822135ce1388d86f
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-12 12:30:31 +0200

    / ‘crowbar/extensions/totop.py’

--- crowbar/extensions/totop.py
+++ crowbar/extensions/totop.py
@@ -1,3 +1,17 @@
-from extension import Extension
+from crowbar.extension import Extension
+from gi.repository import Gtk
+
+Name = "ToTop"
+shortDes = "Go To Top of page",
+Descrption = "and nothing more :P",
+Author = "Jeremi 'jeremi360' Biernacki"
+url = ""
+Icon = Gtk.STOCK_GOTO_TOP
 
 class ToTop(Extension):
+    def __init__(self):
+        super(ToTop, self).__init__(
+                                    Name, shortDes,
+                                    Descrption, Author,
+                                    url, Icon
+                                    )
