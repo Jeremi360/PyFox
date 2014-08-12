09d0afbda222559a83103410d6d5d5f948bc0eef
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-12 16:16:21 +0200

    / ‘crowbar/main.py’

--- crowbar/main.py
+++ crowbar/main.py
@@ -72,7 +72,7 @@ class Window(grabbo.Window):
         hb = Gtk.HeaderBar()
         hb.props.show_close_button = True
         hb.props.title = ""
-        hb.pack_start(G, True)
+        hb.add(G)
         self.set_titlebar(hb)
         hb.show()
 
