21aa36af3f4faaf00fe7f0a28c302ec5f2300529
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-12 11:20:51 +0200

    / ‘crowbar/extensions.py’

--- crowbar/extensions.py
+++ crowbar/extensions.py
@@ -4,11 +4,6 @@ import os
 
 ExtBox_UI = os.path.join('..', 'ui', 'ExtBox.ui')
 
-class ExtBox(Builder):
-    def __init__(self, ext):
-        super(ExtBox, self).__init__(ExtBox_UI)
-
-
 class Extension(object):
     def __init__(self, Name, Descrption, Author, url, Icon):
         self._name = Name
@@ -17,7 +12,32 @@ class Extension(object):
         self._url = url
         self._icon = Icon
 
-        self._box =
+    def get_name(self):
+        return self._name
 
-        self._
+    def get_descrption(self):
+        return self._des
+
+    def get_author(self):
+        return self._aur
+
+    def get_url(self):
+        return self._url
+
+    def get_icon(self):
+        return self._icon
+
+
+class ExtBox(Builder):
+    def __init__(self, ext = Extension):
+        super(ExtBox, self).__init__(ExtBox_UI)
+        self._label = self.ui.get_object("label")
+        self._icon = self.ui.get_object("image")
+        self._switch = self.ui.get_object("switch")
+        self._text = self.ui.get_object("textview")
+
+        self._label Gtk.Label.set_text(ext.)
+
+
+
 
