4137bf646c9c51c9b66d124c60b678ac34f3615e
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-13 09:51:32 +0200

    / ‘crowbar/main.py’

--- crowbar/main.py
+++ crowbar/main.py
@@ -70,8 +70,8 @@ class Window(grabbo.Window):
         self.tabs = Tabs(self, self.G)
         self.tabs.get().show()
 
-        #self.modern() #don't work as i want :(
-        self.old()
+        self.modern() #don't work as i want :(
+        #self.old()
 
         self.show()
 
@@ -79,6 +79,7 @@ class Window(grabbo.Window):
         hb = Gtk.HeaderBar()
         hb.props.show_close_button = True
         hb.set_custom_title(self.G.get())
+        hb.set_has_subtitle(False)
         self.set_titlebar(hb)
         hb.show()
 
