ee8f09b7f8d08284ee517800d6b7701f9cd0217f
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-14 08:29:29 +0200

    / ‘crowbar/main.py’

--- crowbar/main.py
+++ crowbar/main.py
@@ -78,7 +78,6 @@ class Window(grabbo.Window):
         hb.props.border_width = 0
         hb.props.margin = 0
         hb.pack_start(self.G.StartBox)
-        self.G.TabBox.props.resize_mode
         hb.pack_end(self.G.EndBox)
         hb.set_has_subtitle(False)
         self.set_titlebar(hb)
