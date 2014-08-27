7af809338e7049936d08cfee4b2f0d4aa3b9ca10
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-27 11:22:00 +0200

    / ‘crowbar/main.py’

--- crowbar/main.py
+++ crowbar/main.py
@@ -91,7 +91,7 @@ class Window(grabbo.Window):
         self.show()
 
     def modern(self):
-        w = self.get_screen().get_width()
+        w = self.get_window().get_width()
         print(w)
         self.G.set_width(w)
         hb = Gtk.HeaderBar()
