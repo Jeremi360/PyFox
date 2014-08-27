cef4f94ce3499c92d36f93aeeefc1d846a24f081
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-27 10:29:34 +0200

    / ‘crowbar/main.py’

--- crowbar/main.py
+++ crowbar/main.py
@@ -92,9 +92,6 @@ class Window(grabbo.Window):
         hb.set_custom_title(Gtk.Separator())
         hb.props.border_width = 0
         hb.props.margin = 0
-        hb.props.expand = True
-        hb.props.hexpand = True
-        hb.props.hexpand_set = True
         hb.pack_start(self.G.StartBox)
         hb.pack_end(self.G.EndBox)
         hb.set_has_subtitle(False)
