eceee5ca001566e346e6de9cd720e160daf6e40e
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-28 18:07:35 +0200

    / ‘crowbar/tab.py’

--- crowbar/tab.py
+++ crowbar/tab.py
@@ -1,14 +1,14 @@
 from gi.repository import Gtk, WebKit
 import os
-import grabbo
-'''
+
+
 try:
 	import grabbo
 except:
 	print("Please first install Grabbo in your python path or copy to crowbar dir")
 	print("Grabbo can be download from https://github.com/jeremi360/Grabbo")
 	exit()
-'''
+
 
 UI_Tab = os.path.join('..', 'ui', 'Tab.ui')
 
