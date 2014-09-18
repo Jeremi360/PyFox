a21eb37d367e03b35e4a4a8cd16f42f980314cc2
Jeremi Biernacki <jeremi360@gmail.com>
2014-09-18 17:22:43 +0200

    / ‘crowbar/tab.py’

--- crowbar/tab.py
+++ crowbar/tab.py
@@ -147,7 +147,8 @@ class Tab(grabbo.Builder):
 					b.connect("clicked", on_button)
 					HList.add_action_widget(b, 2)
 
-			HList.show()
+			r = self.hist.get_allocation()
+			HList.move_to_coords(r.w, r.h, True)
 
 	def on_full(self, button):
 		if self.fullb.get_active():
