85404b82f426d09d3f0d8b9b01576be8e6fddbae
Jeremi Biernacki <jeremi360@gmail.com>
2014-09-18 15:01:19 +0200

    / ‘crowbar/variable.py’

--- crowbar/variable.py
+++ crowbar/variable.py
@@ -1,1 +1,8 @@
+import os
+
+r = os.path.realpath(__file__)
+r = os.path.dirname(r)
+r = os.path.dirname(r)
+
 home = "https://github.com/jeremi360/cRoWBaR"
+icon = os.path.join(r, 'icons', 'icon.png')
