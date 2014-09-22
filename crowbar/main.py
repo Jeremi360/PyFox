a706387a18b34381d5f0a2ea3e64a02b791a0566
Jeremi Biernacki <jeremi360@gmail.com>
2014-09-22 10:58:29 +0200

    / ‘crowbar/main.py’

--- crowbar/main.py
+++ crowbar/main.py
@@ -64,9 +64,13 @@ class Tabs_Manager(grabbo.Notebook):
 
         if width < wscreen:
             if width < self.minwidth:
-                self.sc.set_min_content_width(self.minwidth)
+                if width > 200:
+                    self.sc.set_min_content_width(self.minwidth*2)
+
+                else:
+                    self.sc.set_min_content_width(self.minwidth)
             else:
-                self.sc.set_min_content_width(width)
+                self.sc.set_min_content_width(width + self.minwidth)
 
         print("end scw", self.get_width())
 
