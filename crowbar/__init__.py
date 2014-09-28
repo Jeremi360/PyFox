3b252a33eb76c223cee6cf4d9ddcf27c3c7c4e0d
Jeremi Biernacki <jeremi360@gmail.com>
2014-09-28 15:48:03 +0200

    / ‘crowbar/__init__.py’

--- crowbar/__init__.py
+++ crowbar/__init__.py
@@ -4,3 +4,4 @@ from crowbar.menu import Menu
 from crowbar.variable import *
 from crowbar.main_controls import MainControls
 from crowbar.aboutd import AboutD
+from crowbar.notebook import Notebook
