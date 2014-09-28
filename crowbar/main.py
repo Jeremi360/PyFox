6c25b7fe99f4ca34a1e050c7ba90f0f159d8d336
Jeremi Biernacki <jeremi360@gmail.com>
2014-09-28 12:14:37 +0200

    / ‘crowbar/main.py’

--- crowbar/main.py
+++ crowbar/main.py
@@ -9,24 +9,23 @@ import grabbo
 class Main(grabbo.Window):
     def __init__(self):
         grabbo.Window.__init__(self)
-        self.MC = crowbar.Main_Controls(self)
+        self.MC = crowbar.MainControls(self)
         self.set_icon_from_file(crowbar.icon)
 
-        #self.tabs = crowbar.Tabs_Manager(self.MC)
+        self.tabs = crowbar.TabControls(self.MC)
         self.tabs.add_tab(url = crowbar.home, active = True)
 
         self.hb = Gtk.HeaderBar()
         self.hb.set_show_close_button(True)
         self.hb.set_title(crowbar.appname)
-        self.hb.set_custom_title(self.tabs.get())
         self.hb.props.border_width = 0
         self.hb.props.margin = 0
-        self.hb.pack_start(self.MC.StartBox)
+        self.hb.pack_start(self.MC.menub)
         self.hb.pack_end(self.MC.EndBox)
         self.hb.set_has_subtitle(False)
         self.set_titlebar(self.hb)
 
-        self.add(self.tabs.stack)
+        self.add(self.tabs)
 
         self.hb.show()
         self.tabs.stack.show()
