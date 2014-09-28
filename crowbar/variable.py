22e3a09044aa34093e9a1ea11eec94198314100e
Jeremi Biernacki <jeremi360@gmail.com>
2014-09-28 15:46:18 +0200

    / ‘crowbar/variable.py’

--- crowbar/variable.py
+++ crowbar/variable.py
@@ -1,17 +1,15 @@
 import os
 
-def get_appdir():
-    r = os.path.realpath(__file__)
-    r = os.path.dirname(r)
-    r = os.path.dirname(r)
-    return r
+r = os.path.realpath(__file__)
+r = os.path.dirname(r)
+r = os.path.dirname(r)
 
 appname = "cRoWBaR"
 version = "0.2.2"
 comment = "Rethinked Web BRowser"
 home = "https://github.com/jeremi360/cRoWBaR"
 rapport = "https://github.com/jeremi360/cRoWBaR/issues"
-icon = os.path.join(get_appdir(), 'icons', 'icon.png')
+icon = os.path.join(r, 'icons', 'icon.png')
 
 authors = [
            "Jeremi 'jeremi360' Biernacki"
