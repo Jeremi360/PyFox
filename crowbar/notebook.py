abb717bcc616671afa4504c015db6f09f6adfda8
Jeremi Biernacki <jeremi360@gmail.com>
2014-09-29 10:12:12 +0200

    / ‘crowbar/notebook.py’

--- crowbar/notebook.py
+++ crowbar/notebook.py
@@ -84,7 +84,7 @@ class Notebook(Gtk.Notebook):
             self.rgroup.append(ts.get_group())
 
         if active:
-            self.ts.button.do_pressed()
+            sts.button.do_pressed()
 
         self.auto_show_switcher()
 
