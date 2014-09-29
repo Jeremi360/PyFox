dacb751d740086ee96216d792aac6606748421e7
Jeremi Biernacki <jeremi360@gmail.com>
2014-09-29 09:11:14 +0200

    / ‘crowbar/notebook.py’

--- crowbar/notebook.py
+++ crowbar/notebook.py
@@ -12,13 +12,13 @@ TabB_UI = os.path.join(r, "ui", "TabButton.xml")
 class TabButton (grabbo.Builder):
     def __init__(self, notebook):
         grabbo.Builder.__init__(self, TabB_UI)
-        self.Label = self.ui.get_object("label")
-        self.Icon = self.ui.get_object("icon")
-        self.Button = self.ui.get_object("button")
+        self.TabB = self.ui.get_object("TabbButton")
+        self.removeB = self.ui.get_object("RemoveButton")
+
         self.num = 0
         self.notebook = notebook
 
-        self.Button.connect("clicked", self.on_button)
+        self.TabB.connect("toggled", self.on_tab)
 
     def set_num(self, num):
         self.num = num
@@ -26,7 +26,10 @@ class TabButton (grabbo.Builder):
     def get(self):
         return self.ui.get_object("box")
 
-    def on_button(self, button):
+    def on_tab(self, button):
+        self.notebook.set_current_page(self.num)
+
+    def on_remove(self, button):
         self.notebook.remove_page(self.num)
 
 class SwVpWv(Gtk.ScrolledWindow):
@@ -42,9 +45,6 @@ class Notebook(Gtk.Notebook):
         Gtk.Notebook.__init__(self)
         self.tabc = tabcontrols
         self.get_show_tabs(False)
-
-
-
 
 
 
