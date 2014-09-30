ab9fa3f6813809b8d76488225bd10be907e66599
Jeremi Biernacki <jeremi360@gmail.com>
2014-09-30 20:29:38 +0200

    / ‘crowbar/tab_controls.py’

--- crowbar/tab_controls.py
+++ crowbar/tab_controls.py
@@ -195,21 +195,6 @@ class TabControls(grabbo.Builder):
 
 		self.webview.load_uri(url)
 
-	def make_short(self, title, lenght = 26):
-		short = ""
-
-		if len(title) > lenght:
-			for i in range(lenght):
-				try:
-					short += title[i]
-				except:
-					pass
-
-		else:
-			short = title
-
-		return short
-
 	def load_icon(self, webview, url):
 		try:
 			pixbuf = webview.get_icon_pixbuf()
