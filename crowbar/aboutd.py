89cd25e548df1c55b124cd34a0a5ab1360762262
Jeremi Biernacki <jeremi360@gmail.com>
2014-10-08 09:04:40 +0200

    / ‘crowbar/aboutd.py’

--- crowbar/aboutd.py
+++ crowbar/aboutd.py
@@ -7,15 +7,15 @@ class AboutD(grabbo.AboutDialog):
     def __init__(self, notebook):
         grabbo.AboutDialog.__init__(self)
         self.notebook = notebook
-        self.set_about_text(crowbar.abouttxt)
-        self.set_appname(crowbar.appname)
+        self.set_about_text(crowbar.aboutText)
+        self.set_appname(crowbar.appName)
         self.set_shortdescrpition(crowbar.comment)
         self.set_home_page(crowbar.home)
         self.set_rapport_page(crowbar.rapport)
         self.set_version(crowbar.version)
-        self.set_license_custom(crowbar.licensetxt)
+        self.set_license_custom(crowbar.licenseText)
         self.set_logo_from_file(crowbar.icon)
-        self.set_title("About " + crowbar.appname)
+        self.set_title("About " + crowbar.appName)
 
     def open_link(self, url):
         self.notebook.add_tab(url, True)
