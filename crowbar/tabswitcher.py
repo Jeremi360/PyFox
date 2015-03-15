c9839b1238df29a3c7323322fedd822c36186a26
jeremi360 <jeremi360@gmail.com>
2015-03-15 17:20:14 +0100

    / ‘crowbar/tabswitcher.py’

--- crowbar/tabswitcher.py
+++ crowbar/tabswitcher.py
@@ -2,7 +2,6 @@ import os #, sys
 #sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 import grabbo
 import crowbar
-from gi.repository import Gtk
 
 r = os.path.realpath(__file__)
 r = os.path.dirname(r)
