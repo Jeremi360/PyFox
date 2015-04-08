18e70f7033640816bd67424ed7fee521b171e92e
jeremi360 <jeremi360@gmail.com>
2015-04-08 22:38:57 +0200

    / ‘crowbar/variable.py’

--- crowbar/variable.py
+++ crowbar/variable.py
@@ -5,7 +5,7 @@ r = os.path.dirname(r)
 r = os.path.dirname(r)
 
 appName = "cRoWBaR"
-version = "0.07.16"
+version = "0.07.18"
 comment = "Rethinked Web BRowser"
 home = "https://github.com/jeremi360/cRoWBaR"
 rapport = "https://github.com/jeremi360/cRoWBaR/issues"
